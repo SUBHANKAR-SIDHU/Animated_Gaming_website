ab58a56d1c44e9d04c12851b2526c574f2e95600
SUBHANKAR SIDHU <subhankarsiddhu@gmail.com>
2026-06-26 21:32:50 +0530

    create hero component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <main className='relative min-h-screen w-screen'>
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
